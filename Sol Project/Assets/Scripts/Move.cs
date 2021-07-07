c19d838afd9a3eee87d3175f5fc40130c9f43494
Dhayson <diego50094@gmail.com>
2021-07-07 18:45:36 -0300

    Visual Studio Working

---
+++ Sol Project/Assets/Scripts/Move.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    private Rigidbody2D rig;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rig = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+    }
+}
